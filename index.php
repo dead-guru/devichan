882991532f76567307757fa45cdfe7b9bd0e68f4
Assada <assada.ua@gmail.com>
2022-12-14 23:24:08 +0200

    random 404 images

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 require 'inc/bootstrap.php';
 
+$dir = "static/404/";
+$files = scandir($dir);
+$images = array_diff($files, array('.', '..'));
+$name = $images[array_rand($images)];
+
 echo Element($config['file_page_template'], [
     'config' => $config,
     'title' => _('404 Not Found'),
     'boardlist' => createBoardlist(),
-    'body' => '<img style="margin: 0 auto;max-width: 700px;display: block;" alt="404" src="/static/404.png">'
+    'body' => '<img style="margin: 0 auto;max-width: 700px;display: block;" alt="404" src="/static/404/' . $name . '">'
 ]);
