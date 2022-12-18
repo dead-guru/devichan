4d4bbc6d1fb31ce2d7709a8a64a91b4c34646b11
Assada <assada.ua@gmail.com>
2022-12-18 22:26:04 +0200

    clean up code resolve #7

--- b.php
+++ b.php
@@ -1,21 +1,40 @@
 <?php
-$dir = "static/banners/";
-$files = scandir($dir);
-$images = array_diff($files, array('.', '..'));
+
+function endsWith($haystack, $needles)
+{
+    if (!is_iterable($needles)) {
+        $needles = (array)$needles;
+    }
+    
+    foreach ($needles as $needle) {
+        if ((string)$needle !== '' && str_ends_with($haystack, $needle)) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+$dir = 'static/banners/';
+
+if (array_key_exists('board', $_GET) && is_dir($dir . ltrim($_GET['board'], '/'))) {
+    $dir .= ltrim($_GET['board'], '/');
+}
+
+$images = array_filter(array_diff(scandir($dir), ['.', '..']), static function ($file) {
+    return endsWith($file, ['.gif', '.jpg', '.jpeg', '.png', '.webp', '.svg', '.apng']);
+});
+
 $name = $images[array_rand($images)];
-// open the file in a binary mode
 $fp = fopen($dir . $name, 'rb');
 $type = mime_content_type($dir . $name);
 
 $bytes = filesize($dir . $name);
-// send the right headers
+
 header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1
 header('Pragma: no-cache'); // HTTP 1.0
 header('Expires: 0'); // Proxies
 header('Content-Type: ' . $type);
 header('Content-Length: ' . $bytes);
 
-// dump the picture and stop the script
 fpassthru($fp);
-exit;
-?>
