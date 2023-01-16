03409c76b232bb1ce8ec8ad20ac40dd39bada036
Assada <assada.ua@gmail.com>
2023-01-16 18:13:48 +0200

    Fix UA localization

--- locale.php
+++ locale.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 function init_locale($locale, $error='error') {
     if (extension_loaded('gettext')) {
+        echo 'ext' . PHP_EOL;
         if (setlocale(LC_ALL, $locale) === false) {
             echo('The specified locale (' . $locale . ') does not exist on your platform!');
         }
@@ -9,6 +10,7 @@ function init_locale($locale, $error='error') {
         bind_textdomain_codeset('tinyboard', 'UTF-8');
         textdomain('tinyboard');
     } else {
+        echo 'func' . PHP_EOL;
         if (_setlocale(LC_ALL, $locale) === false) {
             echo('The specified locale (' . $locale . ') does not exist on your platform!');
         }
@@ -17,7 +19,7 @@ function init_locale($locale, $error='error') {
         _textdomain('tinyboard');
     }
 }
-
+echo locale_get_default() . PHP_EOL;
 init_locale('uk_UA.utf8');
 echo locale_get_default() . PHP_EOL;
 echo gettext("Start a New Thread");
