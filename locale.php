c8e55726284c897f1cd5dc2473bfbc14ce5d5b1a
Assada <assada.ua@gmail.com>
2023-01-16 18:30:57 +0200

    Fix UA localization

--- locale.php
+++ locale.php
@@ -1,28 +1,24 @@
 <?php
 declare(strict_types=1);
-function init_locale($locale, $error='error') {
-    if (extension_loaded('gettext')) {
-        echo 'ext' . PHP_EOL;
-        putenv("LANG=".$locale);
-        if (setlocale(LC_ALL, $locale) === false) {
-            echo('The specified locale (' . $locale . ') does not exist on your platform!');
-        }
-        bindtextdomain('tinyboard', './inc/locale');
-        bind_textdomain_codeset('tinyboard', 'UTF-8');
-        textdomain('tinyboard');
-    } else {
-        echo 'func' . PHP_EOL;
-        if (_setlocale(LC_ALL, $locale) === false) {
-            echo('The specified locale (' . $locale . ') does not exist on your platform!');
-        }
-        _bindtextdomain('tinyboard', './inc/locale');
-        _bind_textdomain_codeset('tinyboard', 'UTF-8');
-        _textdomain('tinyboard');
+function init_locale($locale) {
+    $res = '';
+    putenv("LANG=".$locale);
+    if (setlocale(LC_ALL, $locale) === false) {
+        echo('The specified locale (' . $locale . ') does not exist on your platform!');
     }
+    $res .= bindtextdomain('tinyboard', './inc/locale');
+    $res .= PHP_EOL;
+    $res .= bind_textdomain_codeset('tinyboard', 'UTF-8');
+    $res .= PHP_EOL;
+    $res .= textdomain('tinyboard');
+    $res .= PHP_EOL;
+    $res .= PHP_EOL;
+    
+    return $res;
 }
 
-echo locale_get_default() . PHP_EOL;
-init_locale('uk_UA.utf8');
-echo locale_get_default() . PHP_EOL;
+echo locale_get_default() . PHP_EOL. PHP_EOL;
+echo init_locale('uk_UA.utf8');
+echo locale_get_default() . PHP_EOL . PHP_EOL;
 var_dump($_ENV['HTTP_ACCEPT_LANGUAGE']);
 echo gettext("Start a New Thread");
