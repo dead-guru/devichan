a71902afa5d2800efa0687c25b58b3c9653cb96e
assada <assada.ua@gmail.com>
2026-08-31 20:13:20 +0200

    fix(maintenance): clean files from JSON post data

--- tools/delete-stray-images.php
+++ tools/delete-stray-images.php
@@ -13,22 +13,29 @@ require dirname(__FILE__) . '/inc/cli.php';
 
 $boards = listBoards();
 
-foreach ($boards as $board) {
-	echo "/{$board['uri']}/... ";
+foreach ($boards as $board_info) {
+	echo "/{$board_info['uri']}/... ";
 	
-	openBoard($board['uri']);
+	openBoard($board_info['uri']);
 	
-	$query = query(sprintf("SELECT `file`, `thumb` FROM ``posts_%s`` WHERE `file` IS NOT NULL", $board['uri']));
+	$query = query(sprintf("SELECT `files` FROM ``posts_%s`` WHERE `files` IS NOT NULL", $board['uri']));
 	$valid_src = array();
 	$valid_thumb = array();
 	
 	while ($post = $query->fetch(PDO::FETCH_ASSOC)) {
-		$valid_src[] = $post['file'];
-		$valid_thumb[] = $post['thumb'];
+		$files = json_decode($post['files'], true);
+		if (!is_array($files))
+			continue;
+		foreach ($files as $file) {
+			if (isset($file['file']) && $file['file'] !== 'deleted')
+				$valid_src[] = $file['file'];
+			if (isset($file['thumb']) && !in_array($file['thumb'], array('deleted', 'spoiler'), true))
+				$valid_thumb[] = $file['thumb'];
+		}
 	}
 	
-	$files_src = array_map('basename', glob($board['dir'] . $config['dir']['img'] . '*'));
-	$files_thumb = array_map('basename', glob($board['dir'] . $config['dir']['thumb'] . '*'));
+	$files_src = array_map('basename', glob($board['dir'] . $config['dir']['img'] . '*') ?: array());
+	$files_thumb = array_map('basename', glob($board['dir'] . $config['dir']['thumb'] . '*') ?: array());
 	
 	$stray_src = array_diff($files_src, $valid_src);
 	$stray_thumb = array_diff($files_thumb, $valid_thumb);
@@ -40,7 +47,7 @@ foreach ($boards as $board) {
 	
 	foreach ($stray_src as $src) {
 		$stats['deleted']++;
-		$stats['size'] = filesize($board['dir'] . $config['dir']['img'] . $src);
+		$stats['size'] += filesize($board['dir'] . $config['dir']['img'] . $src);
 		if (!file_unlink($board['dir'] . $config['dir']['img'] . $src)) {
 			$er = error_get_last();
 			die("error: " . $er['message'] . "\n");
@@ -49,7 +56,7 @@ foreach ($boards as $board) {
 		
 	foreach ($stray_thumb as $thumb) {
 		$stats['deleted']++;
-		$stats['size'] = filesize($board['dir'] . $config['dir']['thumb'] . $thumb);
+		$stats['size'] += filesize($board['dir'] . $config['dir']['thumb'] . $thumb);
 		if (!file_unlink($board['dir'] . $config['dir']['thumb'] . $thumb)) {
 			$er = error_get_last();
 			die("error: " . $er['message'] . "\n");
